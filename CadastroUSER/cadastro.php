cf81fff7d5725873b3f4b5aa4a8fea1a90bcbc4d
João Magdaleno <joaomagdaleno14@gmail.com>
2019-03-29 18:01:01 -0300

    Update
    update no login adm e criação do cadastro user

---
+++ CadastroUSER/cadastro.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+		<title>Cadastro | iBookStore </title>
+	</head>
+	<body>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 offset-md-4">
+				<h2>Cadastro</h2>
+				<form>
+                    <div>
+						<input type="text" placeholder="Nome"required auto_focus/>
+					</div>
+                    <div>
+						<input type="date" placeholder="Idade"required/>
+					</div>
+                    <div>
+						<input type="text" placeholder="Telefone"required autofocus />
+					</div>
+                    <div>
+						<input type="text" placeholder="CPF"required autofocus />
+					</div>
+					<div>
+						<input type="text" placeholder="Email"required/>
+					</div>
+					<div>
+						<input type="password" placeholder="Password" required />
+					</div>
+					<p> <a >Já tem uma conta?</a></p>
+					<button type="submit">Login</button>
+				</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
+	</body>
+</html>
