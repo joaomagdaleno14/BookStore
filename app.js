ff9923fa61f78fbe611f504b50cbca46822552b7
João Magdaleno <joaomagdaleno.14@gmail.com>
2019-04-17 15:36:06 -0300

    Atualizando

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ comment. */
 
 'use strict';
 
-console.log('Here\'s a hidden message');
 
 let today = new Date()
 let formatDate = today.toDateString()
 let selectElement = document.getElementById('date')
 selectElement.innerHTML = formatDate;
+
+console.log('Here\'s a hidden message');
