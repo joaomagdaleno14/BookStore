292dac80d5e12fa977d39c4d5ebe09cc1ed04259
João Magdaleno <joaomagdaleno.14@gmail.com>
2019-04-20 21:09:16 -0300

    atualizando

--- User/Login/login.php
+++ User/Login/login.php
@@ -5,34 +5,22 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-		<title>Cadastro | iBookStore </title>
+		<title>Login | iBookStore </title>
 	</head>
 	<body>
 		<section>
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8 offset-md-4">
-				<h2>Cadastro</h2>
+				<h2>Entrar</h2>
 				<form>
-                    <div>
-						<input type="text" placeholder="Nome"required auto_focus/>
-					</div>
-                    <div>
-						<input type="date" placeholder="Idade"required/>
-					</div>
-                    <div>
-						<input type="text" placeholder="Telefone"required autofocus />
-					</div>
-                    <div>
-						<input type="text" placeholder="CPF"required autofocus />
-					</div>
 					<div>
-						<input type="text" placeholder="Email"required/>
+						<input type="text" placeholder="Email"required autofocus />
 					</div>
 					<div>
 						<input type="password" placeholder="Password" required />
 					</div>
-					<p> <a >Já tem uma conta?</a></p>
+					<p> <a >Não tem uma conta?</a></p>
 					<button type="submit">Login</button>
 				</form>
 					</div>
